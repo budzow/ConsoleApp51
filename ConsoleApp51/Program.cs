48d770ee2f0ae89d89f20b8e1ee930b6418ce19c
Michal Budzowski <michal.budzowski@sonarsource.com>
2022-10-27 16:31:41 +0200

    Add project files.

---
+++ ConsoleApp51/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        void func(bool condition)
+        {
+            if (condition)  // Noncompliant
+                DoTheThing();
+
+            DoTheOtherThing();
+            SomethingElseEntirely();
+
+            Foo();
+        }
+
+        private void Foo()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SomethingElseEntirely()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DoTheOtherThing()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DoTheThing()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
